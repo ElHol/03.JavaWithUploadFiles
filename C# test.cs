9c3ee27dacda77a9cbbaec9789af2b68ec241f77
Maria1609 <maria201115@gmail.com>
2018-11-21 09:35:21 +0200

    Add C# file.

---
+++ C# test.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace _04.ForthTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Java Coffee in C# - test!");
+            
+
+        }
+    }
+}
